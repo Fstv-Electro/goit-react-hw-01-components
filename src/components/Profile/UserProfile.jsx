c1e841c3ac0976418b154b2c3f315fcedadf496c
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2023-02-07 11:43:52 +0200

    added files and code

---
+++ src/components/Profile/UserProfile.jsx
@@ -0,0 +1,42 @@
+// import css from './UserProfile.module.css';
+import propTypes from 'prop-types';
+
+export const User = ({ username, tag, location, avatar, stats }) => {
+    return (
+        <div className="profile">
+        <div className="description">
+            <img
+                src={avatar}
+                alt="User avatar"
+                className="avatar"
+            />
+            <p className="name">{username}</p>
+            <p className="tag">@{tag}</p>
+            <p className="location">{location}</p>
+        </div>
+
+        <ul className="stats">
+            <li>
+                <span className="label">Followers</span>
+                    <span className="quantity">{stats.followers}</span>
+            </li>
+            <li>
+                <span className="label">Views</span>
+                <span className="quantity">{stats.views}</span>
+            </li>
+            <li>
+                <span className="label">Likes</span>
+                <span className="quantity">{stats.likes}</span>
+            </li>
+        </ul>
+    </div>);
+};
+
+
+User.propTypes = {
+    username: propTypes.string.isRequired,
+    tag: propTypes.string.isRequired,
+    location: propTypes.string.isRequired,
+    avatar: propTypes.string.isRequired,
+    stats: propTypes.objectsOf(propTypes.number),
+};
